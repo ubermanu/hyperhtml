8f377e0282c371db0389c954c123896a04140563
ubermanu <manu@paca.happy-dev.fr>
2022-09-23 11:30:25 +0200

    add readme test

--- tests/Integration/CustomElementTest.php
+++ tests/Integration/CustomElementTest.php
@@ -29,4 +29,18 @@ class CustomElementTest extends \PHPUnit\Framework\TestCase
         $h3 = new CustomHeading(3);
         $this->assertEquals('<h3></h3>', $h3->render());
     }
+
+    /**
+     * @covers
+     * @return void
+     */
+    public function testInlineCustomElement(): void
+    {
+        $customElement = new class extends \Ubermanu\PhpElements\Element {
+            protected string $tag = 'p';
+            protected array $attributes = ['title' => 'My custom element'];
+        };
+
+        $this->assertEquals('<p title="My custom element"></p>', $customElement->render());
+    }
 }
